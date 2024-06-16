4523c5e72344ee6e234b9400ca6287cb25e42489
Ekta Joshi <ektajoshi1998@gmail.com>
2024-06-16 11:02:23 +0530

    First React

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id:'heading1', xyz :'abc'}, "Hellooooo");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+console.log(heading)
